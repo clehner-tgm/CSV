3a95c3275a8528153778c89aad62d47b297093f4
Cindy Lehner <clehner@student.tgm.ac.at>
2016-02-14 18:06:34 +0100

    TestKlasse hinzugefügt

---
+++ Test/Test.py
@@ -0,0 +1,21 @@
+__author__ = 'Cindy Lehner'
+
+class Test(object):
+
+    def __init__(self):
+        pass
+
+    def test1(self):
+        pass
+
+    def test2(self):
+        pass
+
+    def test3(self):
+        pass
+
+    def test4(self):
+        pass
+
+    def test5(self):
+        pass
